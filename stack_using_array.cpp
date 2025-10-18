0beea05a8dd5ea398d82090d08294943504f1d0c
shanib-techie <shanibkhan622@gmail.com>
2025-10-18 17:20:58 +0530

    hello

--- stack_using_array.cpp
+++ stack_using_array.cpp
@@ -12,16 +12,47 @@ class stack {
     }
     void push(int data){
         if(this->top == this->capacity -1 ){
-            cout<<"overflow";
+            cout<<"overflow\n";
             return;
         }
         this->top++;
         this->arr[this->top] =data;
     }
     int pop (){
-        if(t)
+        if(this->top == -1){
+            cout<<"UNDERFLOW\n";
+            return -45678;
+        }
+        return this->arr[this->top];
+    }
+        int getpop (){
+        if(this->top == -1){
+            cout<<"UNDERFLOW\n";
+            return -45678;
+        }
+        return this->arr[this->top];
+    }
+    bool isEmpty(){
+        return this->top == -1;
+    }
+    int size(){
+        return this->top + 1;
+    }
+    bool isFull(){
+        return this->top == this->capacity -1;
     }
 };
 int main(){
-    cout<<"hekk";
+   stack st(5);
+   st.push(1);
+   st.push(2);
+   st.push(3);
+   cout<<st.getpop()<<"\n";
+   st.push(4);
+   st.push(5);
+   cout<<st.getpop()<<"\n";
+   st.push(8);
+   st.pop();
+   st.pop();
+   cout<<st.getpop()<<"\n";
 }
