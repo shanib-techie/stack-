60ef8e43162e841e2923eb4d15a989971217a5ad
shanib-techie <shanibkhan622@gmail.com>
2025-10-18 16:25:17 +0530

    make changes

--- stack_using_array.cpp
+++ stack_using_array.cpp
@@ -1,5 +1,27 @@
 #include <iostream>
 using namespace std;
+class stack {
+    int capacity;
+    int* arr;
+    int top;
+    public:
+    stack(int c){
+        this->capacity = capacity;
+        arr  = new int[capacity];
+        this->top = -1;
+    }
+    void push(int data){
+        if(this->top == this->capacity -1 ){
+            cout<<"overflow";
+            return;
+        }
+        this->top++;
+        this->arr[this->top] =data;
+    }
+    int pop (){
+        if(t)
+    }
+};
 int main(){
     cout<<"hekk";
 }
